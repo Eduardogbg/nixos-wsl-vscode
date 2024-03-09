53ab8c8362d6bd2508ccb938cb7bd6d1e5392c25
Eduardo <eduardogbg@gmail.com>
2024-03-09 09:48:03 -0300

    test this

--- flake.nix
+++ flake.nix
@@ -1,11 +1,6 @@
 {
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nix-ml-ops = {
-      inputs.flake-parts.follows = "flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:Preemo-Inc/nix-ml-ops";
-    };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-wsl = {
       inputs.nixpkgs.follows = "nixpkgs";
@@ -17,16 +12,12 @@
     };
   };
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } ({ lib, ... }: {
-    imports =
-      lib.trivial.pipe ./flake-modules [
-        builtins.readDir
-        (lib.attrsets.filterAttrs (name: type: type == "regular" && lib.strings.hasSuffix ".nix" name))
-        builtins.attrNames
-        (builtins.map (name: ./flake-modules/${name}))
-      ] ++
-      [
-        inputs.nix-ml-ops.flakeModules.nixIde
-      ];
+    imports = lib.trivial.pipe ./flake-modules [
+      builtins.readDir
+      (lib.attrsets.filterAttrs (name: type: type == "regular" && lib.strings.hasSuffix ".nix" name))
+      builtins.attrNames
+      (builtins.map (name: ./flake-modules/${name}))
+    ];
 
     flake = flake: {
       nixosConfigurations.nixosWslVsCode = inputs.nixpkgs.lib.nixosSystem {
@@ -36,19 +27,12 @@
           flake.config.nixosModules.vscodeServerWsl
           ({ lib, pkgs, config, ... }: {
             wsl = {
-              enable = true;
-              wslConf.automount.root = "/mnt";
-              defaultUser = "nixos";
               startMenuLaunchers = true;
               useWindowsDriver = true;
             };
 
             hardware.opengl.setLdLibraryPath = true;
 
-            # Enable nix flakes
-            nix.extraOptions = ''
-              experimental-features = nix-command flakes
-            '';
             nix.settings.trusted-users = [ config.wsl.defaultUser ];
             nix.settings.extra-substituters = [
               "https://nix-community.cachix.org"
@@ -61,22 +45,14 @@
               "/usr/lib/wsl"
             ];
 
-            system.stateVersion = "22.05";
-
             environment.defaultPackages = [
               pkgs.cachix
             ];
 
             nixpkgs.config.allowUnfree = true;
-
-            programs.git.enable = true;
-
-            programs.direnv.enable = true;
-
           })
         ];
       };
     };
   });
-
 }
